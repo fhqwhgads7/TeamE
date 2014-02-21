2c79b95b3bcc489d0e819b3b673a5246a1761f6e
Jordan Guymon <jaguymon@gmail.com>
2014-02-20 23:06:47 -0700

    Stylin'
    Styled up the Game Over and Game Board screens to be more uniform with
    the rest, and patched up the Title, New Game, and Load pages

--- js/Shared.js
+++ js/Shared.js
@@ -36,14 +36,14 @@ function checkOptions(){
 	if (!((localStorage.getItem('CPUIntelligence') == 'BEGINNER') || (localStorage.getItem('CPUIntelligence') == 'STANDARD') || (localStorage.getItem('CPUIntelligence') == 'EXPERT')))
 		localStorage.setItem('CPUIntelligence','STANDARD');
 	//Checks if 'NumberOfRounds' is 40, 45, 50, 55, or 60. Sets to '40' if otherwise.
-	if (!((parseInt(localStorage.getItem('NumberOfRounds'))/5 >= 8)&&(parseInt(localStorage.getItem('NumberOfRounds'))/5 <= 12)))
+	if (!((parseInt(localStorage.getItem('NumberOfRounds'))/5 >= 8)&&(parseInt(localStorage.getItem('NumberOfRounds'))/5 <= 12)) || localStorage.getItem('NumberOfRounds')==null)
 		localStorage.setItem('NumberOfRounds','40');
 	//Checks if 'PatentingEnabled' is 'true' or 'false'. Sets to 'true' if otherwise.
 	if (!((localStorage.getItem('PatentingEnabled') == 'true') || (localStorage.getItem('PatentingEnabled') == 'false')))
 		localStorage.setItem('PatentingEnabled','true');
-	//Checks if 'RetirementMinimum' is 401000, 500000, 600000, 700000, 800000, 900000, 1000000, or 999999999. Sets to '401000' if otherwise.
-	if (!((localStorage.getItem('RetirementMinimum') == '401000') || ((parseInt(localStorage.getItem('RetirementMinimum'))/100000 >= 5) && (parseInt(localStorage.getItem('RetirementMinimum'))/100000 <= 10)) || (localStorage.getItem('RetirementMinimum') == '999999999')))
-		localStorage.setItem('RetirementMinimum','401000');
+	//Dummied out: Checks if 'RetirementMinimum' is 401000, 500000, 600000, 700000, 800000, 900000, 1000000, or 999999999. Sets to '401000' if otherwise.
+	//if (!((localStorage.getItem('RetirementMinimum') == '401000') || ((parseInt(localStorage.getItem('RetirementMinimum'))/100000 >= 5) && (parseInt(localStorage.getItem('RetirementMinimum'))/100000 <= 10)) || (localStorage.getItem('RetirementMinimum') == '999999999')))
+	//	localStorage.setItem('RetirementMinimum','401000');
 }
 var PlayerColors={
 	Red:"rgb(255,30,30)",

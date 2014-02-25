bf86a30875429876bb4d4b8ed606e21ef4f30fd1
Ryan Lane <ryan@ryanalane.com>
2014-02-24 19:23:10 -0700

    Setup initial QUnit Javascript unit testing with dummy tests

---
+++ js/tests.js
@@ -0,0 +1,11 @@
+test("failed test", function() {
+    var now = "2008/01/28 22:25:00";
+    okay
+    var value = "szia";
+    equal( value, "hello", "We expect value to be 'hello'" );
+});
+
+test("passed test", function() {
+    var value = "hello";
+    equal( value, "hello", "We expect value to be 'hello'" );
+});

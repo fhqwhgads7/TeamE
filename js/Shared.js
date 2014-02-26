da4330050348bb40447786b970d5d8195082e50e
ajmurill <ajmurill@asu.edu>
2014-02-25 21:14:04 -0700

    checkin of some game functionality
    player, product and game objects are complete
    they have toString functions and all the bookkeeping (game keeping track
    of its players, players keeping track of their products) ought to work,
    though only in one direction (adding items)
    
    the game board initializes, pulls down the info from LS and makes a new
    game object with it, ready to run

--- js/Shared.js
+++ js/Shared.js
@@ -29,23 +29,6 @@ function SwitchToPage(page){
 		window.location=page;
 	},149);
 }
-function checkOptions(){
-	//Checks if 'GameDifficulty' is 'EASY', 'NORMAL', 'HARD', or 'LUNATIC'. Sets to 'NORMAL' if otherwise.
-	if (!((localStorage.getItem('GameDifficulty') == 'EASY') || (localStorage.getItem('GameDifficulty') == 'NORMAL') || (localStorage.getItem('GameDifficulty') == 'HARD') || (localStorage.getItem('GameDifficulty') == 'LUNATIC')))
-		localStorage.setItem('GameDifficulty','NORMAL');
-	//Checks if 'CPUIntelligence' is 'BEGINNER', 'STANDARD', or 'EXPERT'. Sets to 'STANDARD' if otherwise.
-	if (!((localStorage.getItem('CPUIntelligence') == 'BEGINNER') || (localStorage.getItem('CPUIntelligence') == 'STANDARD') || (localStorage.getItem('CPUIntelligence') == 'EXPERT')))
-		localStorage.setItem('CPUIntelligence','STANDARD');
-	//Checks if 'NumberOfRounds' is 40, 45, 50, 55, or 60. Sets to '40' if otherwise.
-	if (!((parseInt(localStorage.getItem('NumberOfRounds'))/5 >= 8)&&(parseInt(localStorage.getItem('NumberOfRounds'))/5 <= 12)) || localStorage.getItem('NumberOfRounds')==null)
-		localStorage.setItem('NumberOfRounds','40');
-	//Checks if 'PatentingEnabled' is 'true' or 'false'. Sets to 'true' if otherwise.
-	if (!((localStorage.getItem('PatentingEnabled') == 'true') || (localStorage.getItem('PatentingEnabled') == 'false')))
-		localStorage.setItem('PatentingEnabled','true');
-	//Dummied out: Checks if 'RetirementMinimum' is 401000, 500000, 600000, 700000, 800000, 900000, 1000000, or 999999999. Sets to '401000' if otherwise.
-	//if (!((localStorage.getItem('RetirementMinimum') == '401000') || ((parseInt(localStorage.getItem('RetirementMinimum'))/100000 >= 5) && (parseInt(localStorage.getItem('RetirementMinimum'))/100000 <= 10)) || (localStorage.getItem('RetirementMinimum') == '999999999')))
-	//	localStorage.setItem('RetirementMinimum','401000');
-}
 var PlayerColors={
 	Red:"rgb(255,30,30)",
 	Green:"rgb(0,170,0)",
@@ -68,27 +51,6 @@ var NumberNameTable={
 	5:"Five",
 	6:"Six"
 }
-function Product(player,name,category,sub,color){
-	this.Name=name;
-	this.Category=category;
-	this.SubCategory=sub;
-	this.Color=color;
-	this.IdeaStrength=0;
-	this.DesignStrength=0;
-	this.BuildStrength=0;
-	this.Phase="nil";
-	if(player != null){
-		player.Products.push(this);
-		this.Number=player.Products.length-1;
-		this.OwnerNumber=player.Number;
-		player.NumProducts=player.Products.length;
-	}
-	this.DisplayItemID="nil";
-	this.toString = function () {
-		return this.Name + ', ' + this.Category + ', ' + this.SubCategory + ', ' + this.Color;
-	}
-	return this;
-}
 function Background(){
     day=new Date()
     x=day.getHours()
@@ -102,7 +64,7 @@ function Background(){
         $("#MainBackground").css("background-image", 'url(../images/background_noon_1.jpg)')
         //body.style.backgroundImage = 'url(background_noon_1.jpg)';
     }
-    else if(x>=15 && x<21) {
+    else if(x>=15 && x<20) {
         //var body = document.getElementsByTagName('MainBackground')[0];
         $("#MainBackground").css("background-image", 'url(../images/background_afternoon_1.jpg)')
         //body.style.backgroundImage = 'url(background_afternoon_1.jpg)';
@@ -160,4 +122,70 @@ function MakeBGM(){
 			});
 			//nowPlaying.pause();
 		});	
+}
+//-- CLASS DEFINITIONS --//
+function Product(player,name,category,sub,color){
+	this.ClassName="PRODUCT";
+	this.Name=name;
+	this.Category=category;
+	this.SubCategory=sub;
+	this.Color=color;
+	this.IdeaStrength=0;
+	this.DesignStrength=0;
+	this.BuildStrength=0;
+	this.Phase="nil";
+	if(player != null){
+		player.Products.push(this);
+		this.Number=player.Products.length-1;
+		this.OwnerNumber=player.Number;
+		this.Owner=player;
+		player.NumProducts=player.Products.length;
+		player.ParentGame.PlayerProducts.push(this);
+		this.GlobalID=player.ParentGame.PlayerProducts.length;
+	}else{
+		alert("Created product without an owner!");
+	}
+	this.DisplayItemID="nil";
+	this.toString = function () {
+		return this.ClassName+" "+this.GlobalID+": "+this.Owner.Name+", "+this.Category+", "+this.SubCategory+", "+this.Color;
+	}
+	return this;
+}
+function Player(game,name,type,color){
+	this.ClassName="PLAYER";
+	this.Name=name;
+	this.Color=color;
+	this.Type=type;
+	if(game != null){
+		game.Players.push(this);
+		game.NumPlayers=game.Players.length;
+		this.Number=game.NumPlayers;
+		this.GlobalID=this.Number;
+	}else{
+		alert("Created player without a parent game!");
+	}
+	this.toString = function () {
+		return this.ClassName+" "+this.GlobalID+": "+this.Name+", "+this.Type+", "+this.Color;
+	}
+	return this;
+}
+function Game(id){
+	this.ClassName="GAME_OBJECT";
+	this.ID=id;
+	this.Players={};
+	this.NumPlayers=0;
+	this.PlayerProducts={};
+	this.CurrentPlayer=null;
+	this.CurrentPlayerNum=0;
+	this.Settings={
+		Difficulty:null,
+		CPUIntelligence:null,
+		PatentingEnabled:null,
+		NumberOfRounds:null
+	}
+	this.GameState="Uninitialized"; // this game-state is going to become very important when we start the networking
+	this.toString=function(){
+		return this.ClassName+" "+this.ID.toString();
+	}
+	return this;
 }

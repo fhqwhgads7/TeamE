52a4fa2554ca50516577cdd16daebe79320bb4dd
Jordan Guymon <jaguymon@gmail.com>
2014-03-24 02:13:13 -0700

    Patented Unit Tests
    Wrote a test block to check if Patenting functions work properly.
    Everything seems to be in order.

--- js/tests.js
+++ js/tests.js
@@ -30,3 +30,74 @@ test("Game options and values properly stored", function(){
 	
 });
 */
+
+
+test("patenting functions work properly", function(){
+	
+	var testGame = new Game("2947");
+	ok(testGame, "The Test Game object exists.");
+	
+	var PlayerOne = new Player(testGame, "AwwYiss", "Player", "Red");
+	var PlayerTwo = new Player(testGame, "MFin", "Player", "Blue");
+	var PlayerThree = new Player(testGame, "UnitTest", "Player", "Green");
+	testGame.PatentTracker = new PatentTracker();
+	
+	ok(testGame.PatentTracker, "This Patent Tracker exists and is not deformed.");
+	
+	var Product_1_1 = new Product(PlayerOne, "Dummy1", "Energy", "Hydroelectric", PlayerOne.Color);
+	var Product_1_2 = new Product(PlayerOne, "Dummy2", "Other", "Key Holder", PlayerOne.Color); 
+	var Product_2_1 = new Product(PlayerTwo, "Dummy3", "Energy", "Hydroelectric", PlayerTwo.Color);
+	var Product_2_2 = new Product(PlayerTwo, "Dummy4", "Software", "Video Game", PlayerTwo.Color);
+	var Product_3_1 = new Product(PlayerThree, "Dummy5", "Energy", "Hydroelectric", PlayerThree.Color);
+	var Product_3_2 = new Product(PlayerThree, "Dummy6", "Energy", "Hydroelectric", PlayerThree.Color);
+	var Product_3_3 = new Product(PlayerThree, "Dummy7", "Other", "Key Holder", PlayerThree.Color);
+	var categoryIndex = 0;
+	testGame.CurrentPlayer = PlayerThree;
+
+	ok(TryToBuyPatent(Product_3_1, testGame), "The function for buying patents doesn't break.");
+	ok((TryToBuyPatent(Product_3_1, testGame).indexOf("impressive enough") > -1), "This product isn't impressive enough just yet.");
+	Product_3_1.IdeaStrength=10;
+	Product_3_1.DesignStrength=10;
+	Product_3_1.BuildStrength=10;
+	PlayerThree.Money=0;
+	ok((TryToBuyPatent(Product_3_1, testGame).indexOf("short") > -1), "More money should be required.");
+	PlayerThree.Money=9015;
+	
+	//To simulate the patenting success. (The function to patent manipulates html elements, which cannot be done during unit tests.)
+	categoryIndex = testGame.PatentTracker.Categories.indexOf(Product_3_1.SubCategory);
+	testGame.PatentTracker.Records[categoryIndex][1] = PlayerThree.GlobalID;
+	testGame.PatentTracker.Records[categoryIndex][2] = Product_3_1.GlobalID;
+	
+	ok((TryToBuyPatent(Product_3_2, testGame).indexOf("You've already") > -1), "They should already have this patent.");
+	
+	//Player One's turn
+	testGame.CurrentPlayer = PlayerOne;
+	
+	ok(TryToBuyPatent(Product_1_2, testGame), "Still no breaking.");
+	ok((TryToBuyPatent(Product_1_2, testGame).indexOf("impressive enough") > -1), "This product isn't impressive enough just yet.");
+	ok((TryToBuyPatent(Product_1_1, testGame).indexOf("already has the patent") > -1), "Player Three beat Player One to the punch.");
+	
+	//Simulating Player One patenting their dinky little key holder
+	categoryIndex = testGame.PatentTracker.Categories.indexOf(Product_1_2.SubCategory);
+	testGame.PatentTracker.Records[categoryIndex][1] = PlayerOne.GlobalID;
+	testGame.PatentTracker.Records[categoryIndex][2] = Product_1_2.GlobalID;
+	
+	
+	//Player Two's turn.
+	testGame.CurrentPlayer = PlayerTwo;
+	
+	ok(TryToBuyPatent(Product_2_2, testGame), "Still no breaking.");
+	Product_2_2.isANewProduct = false; //Not a new product.
+	ok((TryToBuyPatent(Product_2_2, testGame).indexOf("on the market") > -1), "This product was on the market and shouldn't be patented.");
+	
+	
+	//ROYAL TESTS
+	equal(doIPayRoyalties(Product_1_1, testGame.PatentTracker), PlayerThree.GlobalID, "Player Three gets royalties.");
+	equal(doIPayRoyalties(Product_1_2, testGame.PatentTracker), -1, "No one gets royalties.");
+	equal(doIPayRoyalties(Product_2_1, testGame.PatentTracker), PlayerThree.GlobalID, "Player Three gets royalties.");
+	equal(doIPayRoyalties(Product_2_2, testGame.PatentTracker), -1, "No one gets royalties.");
+	equal(doIPayRoyalties(Product_3_1, testGame.PatentTracker), -1, "No one gets royalties.");
+	equal(doIPayRoyalties(Product_3_2, testGame.PatentTracker), -1, "No one gets royalties.");
+	equal(doIPayRoyalties(Product_3_3, testGame.PatentTracker), PlayerOne.GlobalID, "Player One gets royalties.");
+	
+});

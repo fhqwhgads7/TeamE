5a406b1937ff7bef5aab866550c5df39742e4b23
ajmurill <ajmurill@asu.edu>
2014-04-12 20:10:17 -0700

    virtually intelligent players
    if by "virtually" we mean "not at all", lol
    
    anywho, VI now pays attention to CPUIntelligence, though it's more of at
    this point an "aggressiveness" multiplier than actual intelligence. Will
    fix in future
    
    VI can now close alerts at the beginning of its turn, though it's a
    clumsy implementation that makes some noise.. egh
    
    VI can now select different product categories

--- js/virtual_intelligence.js
+++ js/virtual_intelligence.js
@@ -1,10 +1,17 @@
 // this is a somewhat awkward system to make delayed real-time simulation of a player (virtual intelligence) WITHOUT blocking the rest of the program. JS doesn't have multithreading :/
 var self;
 var ActionQue=[];
-var VISpeed=0.4;
 var NAEMS=["Win","The Awesome Auger","The Big City Slider Station","DC Snowboards","The Ding King","The DualSaw","Engrave-It","ESPN360","The EZ Bundler","The EZ Crunch Bowl","Files Away","The Gopher","The Grabit","The Grater Plater","Green Now!","The Grip Wrench","The Handy Switch","The Hercules Hook","iCan Health Insurance","Impact Gel","The iTie","The Jupiter Jack","Kaboom!","Mighty Mendit","Mighty Shine","Mighty Putty","Mighty Tape","Orange Glo","OxiClean","The Quick Chop","The Samurai Shark","Tool Band-It","The Turbo Tiger","The Ultimate Chopper","The WashMatik","What odor?","Zorbeez","The Slap Chop","ShamWow","The Schticky"]; // R.I.P. Mays
+var VI_WAIT_TIME=1000; // milliseconds before beginning turn actions
+var Vi_diff_mult;
+var VI_DIFFICULTY_MULT_TABLE={
+	"BEGINNER":0.75,
+	"STANDARD":1,
+	"EXPERT":1.25
+}
 function VI_Begin(ply){
 	self=ply;
+	Vi_diff_mult=VI_DIFFICULTY_MULT_TABLE[TheGame.Settings.CPUIntelligence];
 	self.VIMemory.Money=parseInt($("#CurPlyMoney").text());
 	self.VIMemory.NumCreative=parseInt($("#CurPlyDsgns").text());
 	self.VIMemory.NumDevs=parseInt($("#CurPlyDevs").text());
@@ -12,8 +19,7 @@ function VI_Begin(ply){
 	VI_Logic();
 }
 function VI_Logic(){
-	//ToDo: make so can close alerts at beginning of turn
-	//ToDo: make so can choose product categories
+	//ToDo: CPU intelligence is currently more of an aggressiveness multiplier, rather than actual intelligence. Fix this
 	if(self.VIMemory.NumQA==0){
 		VI_Support_MoreEmployees();
 	}
@@ -30,64 +36,65 @@ function VI_Logic(){
 			});
 			ActionQue.push([Id,null,200]);
 			// now for product logic;
-			if(Math.random()<0.33){
+			if(Math.random()<(0.33*Vi_diff_mult)){
 				ActionQue.push(["CurProdAdvanceButton",null,200]);
 			}
 		});
 	}
-	if(Math.random()<0.1){
+	if(Math.random()<(0.15*Vi_diff_mult)){
 		VI_Support_NewProduct();
 	}
-	if(Math.random()<0.1){
+	if(Math.random()<(0.1*Vi_diff_mult)){
 		VI_Support_MoreEmployees();
 	}
 	VI_End();
 }
 function VI_End(){
-	var Time=0;
 	setTimeout(function(){
-		$("#EventButton").trigger("click");
-		$("#TriggerButton").trigger(":click");
-	},100);
-	ActionQue.forEach(function(action){
-		Time=Time+action[2]/VISpeed;
+		var VISpeed=0.5*Vi_diff_mult;
+		var Time=0+VI_WAIT_TIME;
+		for(var i=1;i<5;i++){ // this is so loud
+			setTimeout(function(){
+				$("#EventButton").trigger("click");
+				$("#TriggerButton").trigger(":click");
+			},200*i);
+		}
 		setTimeout(function(){
-			if(typeof(action[0]=="string")){
-				if(action[1]){
-					$("#"+action[0]).val(action[1]);
+		},1010)
+		ActionQue.forEach(function(action){
+			Time=Time+action[2]/VISpeed;
+			setTimeout(function(){
+				if(action[0]=="FUNC_CALL"){
+					action[1]();
 				}else{
-					$("#"+action[0]).trigger("click");
+					if(action[1]){
+						$("#"+action[0]).val(action[1]);
+					}else{
+						$("#"+action[0]).trigger("click");
+					}
 				}
-			}else if(typeof(action[0])=="number"){
-				if(action[0]==1){
-					VI_Support_SetCat();
-				}else if(action[0]==2){
-					VI_Support_SetSubCat();
-				}
-			}else{
-				alert(typeof(action[0]));
-			}
-		},Time);
-	});
-	setTimeout(function(){
-		ActionQue=[]; // wipe the action que
-		$("#EndTurnBtn").click();
-		$("#ControlLock").hide();
-	},Time+500/VISpeed);
+			},Time);
+		});
+		setTimeout(function(){
+			ActionQue=[]; // wipe the action que
+			$("#EndTurnBtn").click();
+			$("#ControlLock").hide();
+		},Time+500/VISpeed);
+	},500)
 }
 function VI_Support_NewProduct(){
 	var Naem=NAEMS[Math.round(Math.random()*NAEMS.length-1)];
 	ActionQue.push(["new-product-button",null,200]);
 	ActionQue.push(["NewProdName",Naem,750]);
-	ActionQue.push([1,null,150]);
-	ActionQue.push([2,null,150]);
+	ActionQue.push(["FUNC_CALL",VI_Support_SetCat,150]);
+	ActionQue.push(["FUNC_CALL",VI_Support_SetSubCat,150]);
 	ActionQue.push(["CreateNewProdConfirm",null,500]);
 	self.VIMemory.Products.push([Naem,new Date().getTime()]);
 }
 function VI_Support_MoreEmployees(){
-	var NewQA=Math.round(Math.random()*Math.random()*10);
-	var NewCreative=Math.round(Math.random()*Math.random()*10);
-	var NewDevs=Math.round(Math.random()*Math.random()*10);
+	var NewQA=Math.round(Math.random()*Math.random()*10*Vi_diff_mult);
+	var NewCreative=Math.round(Math.random()*Math.random()*10*Vi_diff_mult);
+	var NewDevs=Math.round(Math.random()*Math.random()*10*Vi_diff_mult);
 	ActionQue.push(["employee_button",null,300]);
 	for(var i=0;i<NewQA;i++){
 		ActionQue.push(["TesPlus",null,100]);
@@ -101,10 +108,9 @@ function VI_Support_MoreEmployees(){
 	ActionQue.push(["EmployeeDoneButton",null,400]);
 }
 function VI_Support_SetCat(){
-	alert("WHY DOESN'T THIS WORK");
 	document.getElementById("NewProductCategory").selectedIndex=Math.round(Math.random(0,5));
+	$("#NewProductCategory").trigger("change");
 }
 function VI_Support_SetSubCat(){
-	alert("DAMNIT");
 	document.getElementById("NewProductSubCategory").selectedIndex=Math.round(Math.random(0,3));
 }

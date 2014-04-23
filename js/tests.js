3d0f9d32b23b980c35a3c2e333234a10f332191e
Ramtin Nikbakht <ramtinnikbakht@yahoo.com>
2014-04-23 11:13:41 -0700

    Unit tests for login and registration

--- js/tests.js
+++ js/tests.js
@@ -104,7 +104,14 @@ test("the VI initializes properly and knows its own name",function(){
 	ok(Ply.VIMemory,"VI never initialized");
 	ok(Ply.VIMemory.Name=="Test_Player","VI doesn't know its name");
 });
-/*
+
+test("user can actually register properly",function(){
+	
+});
+
+test("user can login properly",function(){
+	
+});/*
 test("music plays",function(){
 	var music=music
 	ok(music, "Music plays.");
